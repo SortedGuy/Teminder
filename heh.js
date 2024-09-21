82406b7e85f780f42e829372292d479a98264856
octa.v.v <giveawayyuh@gmail.com>
2024-09-22 01:03:16 +0300

    Update heh.js

--- heh.js
+++ heh.js
@@ -15,7 +15,7 @@ function updateCountdown() {
     // Set the target time to 3 PM today
     targetTime.setHours(15, 0, 0, 0);
     
-    // If the target time is in the past, set it to 4 PM tomorrow
+    
     if (now > targetTime) {
         targetTime.setDate(targetTime.getDate() + 1);
     }
@@ -28,6 +28,15 @@ function updateCountdown() {
     document.getElementById('countdown-timer').innerHTML =
         `Timp ramas pana la urmatoarea actualizare: ${hours}h ${minutes}m ${seconds}s`;
 }
+
+
+setInterval(updateCountdown, 1000);
+
+
+updateCountdown();
+
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const popup = document.getElementById('popup');
@@ -52,8 +61,30 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// Update countdown every second
-setInterval(updateCountdown, 1000);
+document.addEventListener("DOMContentLoaded", function() {
+    const coll = document.getElementsByClassName("collapsible");
+    
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
 
-// Initial call to set the countdown immediately
-updateCountdown();
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
+
+document.getElementById('email-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    emailjs.sendForm('service_7ytbwmb', 'template_fmikp0m', this)
+        .then(function() {
+            alert('Question sent successfully!');
+        }, function(error) {
+            alert('Failed to send question. Please try again later.');
+        });
+});
